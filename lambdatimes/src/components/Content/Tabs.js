1fc804ff234af2d60d5cff24ff6161c93ce198ba
MrT3313 <rturgeon13@gmail.com>
2019-03-23 12:43:54 -0500

    tab onClick & status update working

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -15,7 +15,17 @@ const Tabs = props => {
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
 
-            {props.tabs.map( tab => <Tab tab={tab}/>)}
+            {
+              props.tabs.map( tab => {
+              return (
+                <Tab 
+                  tab={tab}
+                  selectedTab={props.selectedTab}
+                  selectTabHandler={props.selectTabHandler}
+                />
+              )
+            })
+            }
 
       </div>
     </div>
